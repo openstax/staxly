6073767023ddde11fe64a3d33dad4ba095a46e31
Philip Schatz <phil@cnx.org>
2018-01-24 21:47:46 -0600

    :thought_balloon: say hello

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 module.exports = (robot) => {
-  // Your code here
   console.log('Yay, the app was loaded!')
+
+  robot.on('push', async (context) => {
+    console.log('pushed code')
+  })
+
+  robot.on('issues.opened', async (context) => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    const params = context.issue({body: 'Hello World!'})
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
+
+  robot.on('pull_request_review.submitted', async (context) => {
+    console.log('PR Review Submitted', context.payload)
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
