06f693804a63b6638bc0f4cdfe87124825d0dd4f
Philip Schatz <phil@cnx.org>
2018-03-24 19:05:07 -0500

    all the remaining commands

--- src/automate-project-columns.js
+++ src/automate-project-columns.js
@@ -1,26 +1,125 @@
-// - `new.issue` : When an Issue is created
-// - `new.pullrequest` : When a Pull Request is created
-// - `assigned`: When an Issue or Pull Request is assigned to a user
-// - `reopened`: When an Issue or Pull Request is reopened
-// - `reopened.pullrequest`:
-// - `added_reviewer`: (optional username or array of usernames that need to be added)
-// - `accepted`: When at least one Reviewer Accepted, and there are no Rejections on a Pull request
-// - `merged`: When a Pull Request is merged
-// - `closed.pullrequest`: When a Pull Request is closed
-// - `added_label`: (has one argument, the string representing the name of the label)
-// - `closed.issue`: When an Issue is closed
+// - [x] `new.issue` : When an Issue is created
+// - [x] `new.pullrequest` : When a Pull Request is created
+// - [x] `assigned_to.issue`: When an Issue is assigned to a specific user
+// - [x] `assigned.issue`: When an Issue is assigned to a user (but was not before)
+// - [x] `unassigned.issue`: When an Issue is no longer assigned to a user
+// - [x] `assigned.pullrequest`: When a Pull Request is assigned to a user (but was not before)
+// - [x] `unassigned.pullrequest`: When a Pull Request is no longer assigned to a user
+// - [x] `reopened.issue`: When an Issue is reopened
+// - [x] `reopened.pullrequest`: When a Pull Request is reopened
+// - [x] `added_reviewer`: (optional username or array of usernames that need to be added)
+// - [x] `accepted`: When at least one Reviewer Accepted, and there are no Rejections on a Pull request
+// - [x] `merged`: When a Pull Request is merged
+// - [x] `closed.pullrequest`: When a Pull Request is closed
+// - [x] `added_label`: (has one argument, the string representing the name of the label)
+// - [x] `removed_label`: (has one argument, the string representing the name of the label)
+// - [x] `closed.issue`: When an Issue is closed
+// - [x] `edited.issue`
+// - [x] `milestoned.issue`
+// - [x] `demilestoned.issue`
 
 const {readFileSync} = require('fs')
 const {join: pathJoin} = require('path')
 const yaml = require('js-yaml')
 
+function ALWAYS_TRUE () { return true }
+
 const AUTOMATION_COMMANDS = [
+  { ruleName: 'edited.issue', webhookName: 'issues.edited', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'demilestoned.issue', webhookName: 'issues.demilestoned', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'milestoned.issue', webhookName: 'issues.milestoned', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'new.issue', webhookName: 'issues.opened', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'new.pullrequest', webhookName: 'pull_request.opened', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'reopened.pullrequest', webhookName: 'pull_request.reopened', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'reopened.issue', webhookName: 'issues.reopened', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'closed.issue', webhookName: 'issues.closed', ruleMatcher: ALWAYS_TRUE },
+  { ruleName: 'added_reviewer', webhookName: 'pull_request.review_requested', ruleMatcher: ALWAYS_TRUE }, // See https://developer.github.com/v3/activity/events/types/#pullrequestevent to get the reviewer
   {
-    webhookName: 'issues.labeled',
+    ruleName: 'merged',
+    webhookName: 'pull_request.closed',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      // see https://developer.github.com/v3/activity/events/types/#pullrequestevent
+      return !!context.payload.pull_request.merged
+    }
+  },
+  {
+    ruleName: 'closed.pullrequest',
+    webhookName: 'pull_request.closed',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      // see https://developer.github.com/v3/activity/events/types/#pullrequestevent
+      return !context.payload.pull_request.merged
+    }
+  },
+  {
+    ruleName: 'assigned_to.issue',
+    webhookName: 'issues.assigned',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      if (ruleValue !== true) {
+        return context.payload.assignee.login === ruleValue
+      } else {
+        robot.log.error(`assigned_to.issue requires a username but it is missing`)
+      }
+    }
+  },
+  {
+    ruleName: 'assigned.issue',
+    webhookName: 'issues.assigned',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      return context.payload.issue.assignees.length === 1
+    }
+  },
+  {
+    ruleName: 'unassigned.issue',
+    webhookName: 'issues.unassigned',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      return context.payload.issue.assignees.length === 0
+    }
+  },
+  {
+    ruleName: 'assigned.pullrequest',
+    webhookName: 'pull_request.assigned',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      return context.payload.pull_request.assignees.length === 1
+    }
+  },
+  {
+    ruleName: 'unassigned.pullrequest',
+    webhookName: 'pull_request.unassigned',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      return context.payload.pull_request.assignees.length === 0
+    }
+  },
+  {
     ruleName: 'added_label',
-    ruleMatcher: async function (context, ruleValue) {
+    webhookName: 'issues.labeled',
+    ruleMatcher: async function (robot, context, ruleValue) {
       // labels may be defined by a label or an id (for more persistence)
       return context.payload.label.name === ruleValue || context.payload.label.id === ruleValue
+    }
+  },
+  {
+    ruleName: 'removed_label',
+    webhookName: 'issues.unlabeled',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      return context.payload.label.name === ruleValue || context.payload.label.id === ruleValue
+    }
+  },
+  {
+    ruleName: 'accepted',
+    webhookName: 'pull_request_review.submitted',
+    ruleMatcher: async function (robot, context, ruleValue) {
+      // See https://developer.github.com/v3/activity/events/types/#pullrequestreviewevent
+      // Check if there are any Pending or Rejected reviews and ensure there is at least one Accepted one
+      const {data: reviews} = await context.github.pullRequests.getReviews(context.issue())
+      // Check that there is at least one Accepted
+      const hasAccepted = reviews.filter((review) => review.state === 'APPROVED').length >= 1
+      const hasRejections = reviews.filter((review) => review.state === 'REQUEST_CHANGES').length >= 1
+      const hasPending = reviews.filter((review) => review.state === 'PENDING').length >= 1
+      if (hasAccepted && !hasRejections && !hasPending) {
+        return true
+      } else {
+        return false
+      }
     }
   }
 ]
@@ -84,8 +183,9 @@ module.exports = (robot) => {
     robot.on(webhookName, async function (context) {
       await populateCache(context)
 
-      // Check if we need to move the Issue
-      const cardsForIssue = Object.values(CARD_LOOKUP[context.payload.issue.url] || {})
+      // Check if we need to move the Issue (or Pull request)
+      const issueUrl = context.payload.issue ? context.payload.issue.url : context.payload.pull_request.issue_url
+      const cardsForIssue = Object.values(CARD_LOOKUP[issueUrl] || {})
 
       // At this point we need the cards and columns that need to be moved.
       const matchedColumnInfos = cardsForIssue.map(({projectId, cardId, projectConfig}) => {
@@ -122,7 +222,7 @@ module.exports = (robot) => {
       }).filter((info) => !!info) // remove all the nulls (ruleName not found in this projectConfig)
 
       matchedColumnInfos.forEach(async ({columnInfo, cardId, projectId, ruleValue}) => {
-        if (await ruleMatcher(context, ruleValue)) {
+        if (await ruleMatcher(robot, context, ruleValue)) {
           // Move the Card
           const {data: columns} = await context.github.projects.getProjectColumns({project_id: projectId})
 
