4fae66d370f75cac01ce5a0b721c7488cc91de7c
tom <tom.g.woodward@gmail.com>
2020-01-31 14:04:10 -0500

    maybe actually runs now

--- src/merge-bases.js
+++ src/merge-bases.js
@@ -6,6 +6,8 @@ module.exports = (robot) => {
   ], checkForPrs)
   
   const processPrs = context => ({data}) => {
+    const {owner, repo} = context.repo();
+
     for (const pr in data) {
       logger.info(`updating base for ${pr.number}`)
       context.github.pulls.updateBranch({
@@ -20,15 +22,14 @@ module.exports = (robot) => {
     const {payload} = context
 
     const base = payload.ref.replace(/^refs\/heads\//, '');
-    
-    const {owner, repo} = context.repo()
+    const {owner, repo} = context.repo();
 
     context.github.paginate(context.github.pulls.list({
       owner,
       repo,
       base,
       state: 'open',
-    }), processPrs(context))
+    }), processPrs(context));
   }
 
 }
