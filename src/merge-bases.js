30e4976bac87563163392eec4759fd866337ac2e
tom <tom.g.woodward@gmail.com>
2020-01-31 15:38:37 -0500

    try something

--- src/merge-bases.js
+++ src/merge-bases.js
@@ -25,7 +25,7 @@ module.exports = (robot) => {
     const {owner, repo} = context.repo();
 
     logger.info(`received push event for ${base}`)
-    context.github.paginate(context.github.pulls.getAll.endpoint.merge({
+    context.github.paginate(context.github.pullRequests.getAll.endpoint.merge({
       owner,
       repo,
       base,
