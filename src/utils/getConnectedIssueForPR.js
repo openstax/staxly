9d4f6f416478febc15f423b7e5a750fa2eca6656
tom <tom.g.woodward@gmail.com>
2020-02-05 14:16:31 -0500

    :shirt:

--- src/utils/getConnectedIssueForPR.js
+++ src/utils/getConnectedIssueForPR.js
@@ -1,8 +1,15 @@
 const targetRegexes = [
-  /^(.* )?link: (?<owner>openstax)\/(?<repo>[a-z\-]+)#(?<number>[0-9]+)( .*)?$/,
-  /^(.* )?link: https:\/\/github.com\/(?<owner>openstax)\/(?<repo>[a-z\-]+)\/issues\/(?<number>[0-9]+)( .*)?$/,
-  /^(.* )?link: https:\/\/app.zenhub.com\/workspaces\/[0-9a-z\-]+\/issues\/(?<owner>openstax)\/(?<repo>[a-z\-]+)\/(?<number>[0-9]+)( .*)?$/
-];
+  /*
+   * these are strings to stop the linter from complaining about the named capture groups
+   * which is an error that cannot be suppressed
+   **/
+  /* eslint-disable-next-line */
+  '^(.* )?link: (?<owner>openstax)\/(?<repo>[a-z\-]+)#(?<number>[0-9]+)( .*)?$',
+  /* eslint-disable-next-line */
+  '^(.* )?link: https:\/\/github.com\/(?<owner>openstax)\/(?<repo>[a-z\-]+)\/issues\/(?<number>[0-9]+)( .*)?$',
+  /* eslint-disable-next-line */
+  '^(.* )?link: https:\/\/app.zenhub.com\/workspaces\/[0-9a-z\-]+\/issues\/(?<owner>openstax)\/(?<repo>[a-z\-]+)\/(?<number>[0-9]+)( .*)?$'
+]
 
 /*
  * @argument context.issue (or equivalent)
@@ -11,12 +18,12 @@ const targetRegexes = [
  * @returns {owner: string, repo: string, number: string} | null
  */
 module.exports = async (github, pullInfo) => {
-  const pull = await github.pulls.get(pullInfo);
-  const target = targetRegexes.reduce((result, regex) => result || pull.data.body.match(regex), null);
+  const pull = await github.pulls.get(pullInfo)
+  const target = targetRegexes.reduce((result, regex) => result || pull.data.body.match(regex), null)
 
   if (target) {
-    return target.groups;
+    return target.groups
   }
 
-  return null;
-};
+  return null
+}
