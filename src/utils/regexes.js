61e302943c09d39f31e3310318c5979efc3a12f6
tom <tom.g.woodward@gmail.com>
2020-02-10 15:07:53 -0500

    unwreck long bodies again

--- src/utils/regexes.js
+++ src/utils/regexes.js
@@ -1,9 +1,9 @@
 const whitespaceCharacters = '\\s\\r'
 const newlineCharacters = '\\n\\r'
 const whitespace = `[${whitespaceCharacters}]`
-const beginningOfStringOrNewline = `^([^${newlineCharacters}]*[${newlineCharacters}]+)*`
-const beginningOfStringOrWhitespace = `^([^${whitespaceCharacters}]*[${whitespaceCharacters}]+)*`
-const endOfStringOrWhitespace = `([${whitespaceCharacters}]+[^${whitespaceCharacters}]*)*$`
+const beginningOfStringOrNewline = `^([^${newlineCharacters}]+[${newlineCharacters}]+)*[${newlineCharacters}]*`
+const beginningOfStringOrWhitespace = `^([^${whitespaceCharacters}]+[${whitespaceCharacters}]+)*[${whitespaceCharacters}]*`
+const endOfStringOrWhitespace = `([${whitespaceCharacters}]+[^${whitespaceCharacters}]+)*[${whitespaceCharacters}]*$`
 
 /* eslint-disable-next-line */
 const githubRefGroups = '(?<owner>[a-z\-]+)\/(?<repo>[a-z\-]+)#(?<number>[0-9]+)'
