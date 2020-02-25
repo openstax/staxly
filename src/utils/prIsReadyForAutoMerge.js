9ca90c80ce68dd8767073ab5cb335202654e9781
tom <tom.g.woodward@gmail.com>
2020-02-25 11:23:39 -0500

    add util for auto merge criteria

---
+++ src/utils/prIsReadyForAutoMerge.js
@@ -0,0 +1,28 @@
+const {getPipelineStage} = require('./pipeline');
+const getConnectedIssueForPR = require('./getConnectedIssueForPR')
+
+module.exports = async (github, pullRequest, optionalIssue) => {
+  if (!pullRequest.labels.includes('ready to merge')) {
+    return false;
+  }
+
+  const loadIssue = () => {
+    const linkedIssueParams = getConnectedIssueForPR(pullRequest)
+    return linkedIssueParams && github.issues.get(linkedIssueParams)
+  };
+
+  const issue = optionalIssue || await loadIssue()
+
+  if (!issue) {
+    return false;
+  }
+
+  const review = getPipelineStage(issue.body, name => name.match(/review/i));
+  const reviewComplete = review && review.complete;
+
+  if (!reviewComplete) {
+    return false;
+  }
+
+  return true;
+}
---
+++ test/utils/prIsReadyForAutoMerge.test.js
@@ -0,0 +1,118 @@
+const isReadyForAutoMerge = require('../../src/utils/prIsReadyForAutoMerge')
+
+const completedPipeline = `# pipeline
+2B-REVIEW
+  - [x] Developer approves change
+`;
+
+const incompletePipeline = `# pipeline
+2B-REVIEW
+  - [ ] Developer approves change
+`;
+
+const pipelineWithoutReview = `# pipeline
+2B-ASDF
+  - [ ] Developer approves change
+`;
+
+describe('isReadyForAutoMerge', () => {
+  let github
+  const issue = {
+    body: '',
+    number: 123
+  }
+  const pullRequest = {
+    number: 234,
+    labels: [],
+    base: {
+      repo: {
+        name: 'rex-web',
+        owner: {
+          login: 'openstax'
+        }
+      }
+    }
+  }
+
+  beforeEach(() => {
+    github = {
+      issues: {
+        get: jest.fn()
+      }
+    }
+  })
+
+  test('uses passed in issue', async() => {
+    const result = await isReadyForAutoMerge(
+      github,
+      {...pullRequest, body: 'for: openstax/rex-web#123', labels: ['ready to merge']},
+      {...issue, body: completedPipeline},
+    )
+
+    expect(github.issues.get).not.toHaveBeenCalled();
+    expect(result).toEqual(true);
+  })
+
+  test('fetches issue if not passed', async() => {
+    github.issues.get.mockReturnValue(Promise.resolve({...issue, body: completedPipeline}));
+
+    const result = await isReadyForAutoMerge(
+      github,
+      {...pullRequest, body: 'for: openstax/rex-web#123', labels: ['ready to merge']},
+    )
+
+    expect(github.issues.get).toHaveBeenCalledTimes(1);
+    expect(github.issues.get).toHaveBeenCalledWith({repo: 'rex-web', owner: 'openstax', issue_number: "123"});
+    expect(result).toEqual(true);
+  })
+
+  test('fails if pr is unlinked', async() => {
+    const result = await isReadyForAutoMerge(
+      github,
+      {...pullRequest, body: ''},
+    )
+
+    expect(github.issues.get).not.toHaveBeenCalled();
+    expect(result).toEqual(false);
+  })
+
+  test('fails without label', async() => {
+    const result = await isReadyForAutoMerge(
+      github,
+      {...pullRequest, body: 'for: openstax/rex-web#123'},
+      {...issue, body: completedPipeline},
+    )
+
+    expect(result).toEqual(false);
+  })
+
+  test('fails without pipeline', async() => {
+    const result = await isReadyForAutoMerge(
+      github,
+      {...pullRequest, body: 'for: openstax/rex-web#123', labels: ['ready to merge']},
+      issue,
+    )
+
+    expect(result).toEqual(false);
+  })
+
+  test('fails if pipeline has no review', async() => {
+    const result = await isReadyForAutoMerge(
+      github,
+      {...pullRequest, body: 'for: openstax/rex-web#123', labels: ['ready to merge']},
+      {...issue, body: pipelineWithoutReview},
+    )
+
+    expect(result).toEqual(false);
+  })
+
+  test('fails if pipeline review stage is incomplete', async() => {
+    const result = await isReadyForAutoMerge(
+      github,
+      {...pullRequest, body: 'for: openstax/rex-web#123', labels: ['ready to merge']},
+      {...issue, body: incompletePipeline},
+    )
+
+    expect(result).toEqual(false);
+  })
+})
