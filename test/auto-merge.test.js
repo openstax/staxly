1e37416e8015e740483cdff2661676de6f7413d8
tom <tom.g.woodward@gmail.com>
2020-02-26 16:48:43 -0500

    more tests

--- test/auto-merge.test.js
+++ test/auto-merge.test.js
@@ -21,61 +21,84 @@ describe('auto merge', () => {
     nock.enableNetConnect()
   })
 
-  describe('after check run completes', () => {
-    test('passes', async () => {
-      nock('https://api.github.com')
-        .get('/repos/testowner/testrepo/pulls/2')
-        .reply(200, {number: 2, draft: false, state: 'open'})
+  test('passes after check run completes', async () => {
+    nock('https://api.github.com')
+      .get('/repos/testowner/testrepo/pulls/2')
+      .reply(200, {number: 2, draft: false, state: 'open'})
 
-      nock('https://api.github.com')
-        .put('/repos/testowner/testrepo/pulls/2/merge')
-        .reply(200, {message: 'merged pr'})
+    nock('https://api.github.com')
+      .put('/repos/testowner/testrepo/pulls/2/merge')
+      .reply(200, {message: 'merged pr'})
 
-      prIsReadyForAutoMerge.mockReturnValue(true);
+    prIsReadyForAutoMerge.mockReturnValue(true);
 
-      await app.receive({
-        name: 'check_run.completed',
-        payload: {
-          pull_requests: [{
-            number: 2,
-          }],
-          repository: {
-            name: 'testrepo',
-            owner: {
-              login: 'testowner'
-            }
+    await app.receive({
+      name: 'check_run.completed',
+      payload: {
+        pull_requests: [{
+          number: 2,
+        }],
+        repository: {
+          name: 'testrepo',
+          owner: {
+            login: 'testowner'
           }
         }
-      })
-
-      expect(nock.isDone()).toBe(true)
+      }
     })
 
-    test('does nothing when not ready', async () => {
-      nock('https://api.github.com')
-        .get('/repos/testowner/testrepo/pulls/2')
-        .reply(200, {number: 2, draft: false, state: 'open'})
+    expect(nock.isDone()).toBe(true)
+  })
 
-      prIsReadyForAutoMerge.mockReturnValue(false);
+  test('does nothing when merged', async () => {
+    nock('https://api.github.com')
+      .get('/repos/testowner/testrepo/pulls/2')
+      .reply(200, {number: 2, draft: false, state: 'merged'})
 
-      await app.receive({
-        name: 'check_run.completed',
-        payload: {
-          pull_requests: [{
-            number: 2,
-          }],
-          repository: {
-            name: 'testrepo',
-            owner: {
-              login: 'testowner'
-            }
+    prIsReadyForAutoMerge.mockReturnValue(true);
+
+    await app.receive({
+      name: 'check_run.completed',
+      payload: {
+        pull_requests: [{
+          number: 2,
+        }],
+        repository: {
+          name: 'testrepo',
+          owner: {
+            login: 'testowner'
           }
         }
-      })
-
-      expect(nock.isDone()).toBe(true)
+      }
     })
-  });
+
+    expect(nock.isDone()).toBe(true)
+  })
+
+  test('does nothing when not ready', async () => {
+    nock('https://api.github.com')
+      .get('/repos/testowner/testrepo/pulls/2')
+      .reply(200, {number: 2, draft: false, state: 'open'})
+
+    prIsReadyForAutoMerge.mockReturnValue(false);
+
+    await app.receive({
+      name: 'check_run.completed',
+      payload: {
+        pull_requests: [{
+          number: 2,
+        }],
+        repository: {
+          name: 'testrepo',
+          owner: {
+            login: 'testowner'
+          }
+        }
+      }
+    })
+
+    expect(nock.isDone()).toBe(true)
+  })
 
   test('noops outside whitelist', async () => {
     await app.receive({
