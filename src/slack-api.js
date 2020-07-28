d21a9dcd2127aa87d08ffceb368710aeefb2b0dc
tom <tom.g.woodward@gmail.com>
2020-07-28 16:29:59 -0400

    fixing imports

--- src/slack-api.js
+++ src/slack-api.js
@@ -13,9 +13,8 @@
 // since probot no longer supports robot.on('slack.message')
 // because probot events assume a payload which contains the GitHub installation id
 
-import slackClient from '@slack/client'
+const {RTMClient, WebClient} = require('@slack/client')
 
-const {RTMClient, WebClient} = slackClient;
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN
 const SLACK_GITHUB_INSTALL_ID = process.env.SLACK_GITHUB_INSTALL_ID
 
