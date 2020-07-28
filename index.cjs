9963d276179c341bd63fd5a559e4598884180f23
tom <tom.g.woodward@gmail.com>
2020-07-28 16:29:36 -0400

    clean up index file

--- index.cjs
+++ index.cjs
@@ -1,8 +1,6 @@
-module.exports = (robot) => {
-  import('./src/index.js')
-    .then(module => module.default(robot))
-    .catch(e => {
-      console.error(e)
-      process.exit(1)
-    })
-}
+module.exports = (robot) => import('./src/index.js')
+  .then(module => module.default(robot))
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
