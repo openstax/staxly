325cba9fb357e04b93065a7bc82923ff7be479da
Chris Kline <ckline.tryptic@gmail.com>
2021-07-13 11:39:39 -0400

    remove setMaxListeners

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import slackStuff from './slack-stuff.js'
 const { IGNORE_FOR_TESTING } = process.env
 
 export default (robot, { getRouter }) => {
-  // robot.events.setMaxListeners(100) // Since we use multiple plugins
 
   changelog(robot)
   mergeBases(robot)
